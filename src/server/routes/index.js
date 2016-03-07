a8c00f68ef0fa2953b02f6ccf9835ced0c95e0c1
Kevin Njeru <knjeru@gmail.com>
2016-03-07 15:52:09 -0700

    Project Init

---
+++ src/server/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// GET Route for homepage
+
+
+
+// GET Route for product page
+
+
+
+// GET Route for checkout page
+
+
+
+// GET Route for About page
+
+
+
+
+module.exports = router;
