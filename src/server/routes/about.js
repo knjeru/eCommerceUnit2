3e006ca56c82795b27be846ebc315dbe517742ab
Kevin Njeru <knjeru@gmail.com>
2016-03-08 14:01:33 -0700

    Routes/Renders for About and product

--- src/server/routes/about.js
+++ src/server/routes/about.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
 // GET Route for About page
+
+router.get('/about', function(req,res,next) {
+    res.render('about');
+});
+
+module.exports = router;
--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
 // GET Route for product page
+
+router.get('/product', function(req,res,next) {
+    res.render('products');
+});
+
+module.exports = router;
