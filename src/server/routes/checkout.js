0150683a0048a3dcacb97f62abb883f234b295c2
Kevin Njeru <knjeru@gmail.com>
2016-03-10 12:50:09 -0700

    Rudimentary stripe form

--- src/server/routes/checkout.js
+++ src/server/routes/checkout.js
@@ -3,14 +3,26 @@ var router = express.Router();
 
 // GET Route for checkout page
 router.get('/', function(req, res, next) {
-  res.render('checkout',{ title: 'RAD PEOPLE CHECKOUT' });
+  res.render('charge', { title: 'RAD PEOPLE CHECKOUT' });
 });
 
-router.post('/', function(req,res,next) {
-  knex('products').insert({
-    'price': req.body.price,
-    'size': req.body.size
-  });
+router.post('/charge', function(req, res,next) {
+    var stripeToken = req.body.stripeToken;
+    var amount =  req.body.stripeAmount;
+
+    stripe.charges.create({
+        card: stripeToken,
+        currency: 'usd',
+        amount: amount
+    },
+    function(err, charge) {
+        if (err) {
+            res.send('error');
+        } else {
+            res.send('success');
+        }
+    });
+    res.redirect('/');
 });
 
 
